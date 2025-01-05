f9bba6a2d6e1b219781aef36a448b2c0d42dd148
tinda16 <tindaazahra2018@gmail.com>
2025-01-05 21:28:41 +0700

    RevisiSettingBlInstru

--- resources/views/instructor/setting.blade.php
+++ resources/views/instructor/setting.blade.php
@@ -1,6 +1,18 @@
 @extends('instructor.layouts.main')
 @section('title', 'Settings')
 @section('profile')
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+
 	<div class="col-xl-12">
 		<!-- Edit profile START -->
 		<div class="card bg-transparent border rounded-3">
@@ -11,8 +23,9 @@
 			<!-- Card body START -->
 			<div class="card-body">
 				<!-- Form -->
-				<form class="row g-4">
-
+				<form class="row g-4" method="POST" action="{{ route('instructor.profile.update', Auth::user()->id) }}">
+					@csrf
+					@method('PUT')
 					<!-- Profile picture -->
 					<div class="col-12 justify-content-center align-items-center">
 						<label class="form-label">Profile picture</label>
@@ -36,7 +49,7 @@
 					<div class="col-12">
 						<label class="form-label">Full name</label>
 						<div class="input-group">
-							<input type="text" name="name" class="form-control" value="Lori" placeholder="First name">
+							<input type="text" name="full_name" class="form-control" value="{{ $user['full_name'] }}" placeholder="First name">
 						</div>
 					</div>
 
@@ -45,32 +58,32 @@
 						<label class="form-label">Username</label>
 						<div class="input-group">
 							<span class="input-group-text">Eduport.com</span>
-							<input type="text" name="username" class="form-control" value="loristev">
+							<input type="text" name="username" class="form-control" value="{{ $user['username'] }}">
 						</div>
 					</div>
 
 					<!-- Email id -->
 					<div class="col-md-6">
 						<label class="form-label">Email</label>
-						<input class="form-control" type="email" name="email" value="example@gmail.com" placeholder="Email">
+						<input class="form-control" type="email" name="email" value="{{ $user['email'] }}" placeholder="Email">
 					</div>
 
 					<!-- Phone number -->
 					<div class="col-md-6">
 						<label class="form-label">Phone number</label>
-						<input type="text" class="form-control" name="phone_number" value="1234567890" placeholder="Phone number">
+						<input type="text" class="form-control" name="phone_number" value="{{ $user['phone_number'] }}" placeholder="Phone number">
 					</div>
 
 					<!-- About me -->
 					<div class="col-12">
 						<label class="form-label">About me</label>
-						<textarea name="about" class="form-control" rows="3">I’ve found a way to get paid for my favorite hobby, and do so while following my dream of traveling the world.</textarea>
+						<textarea name="about" class="form-control" rows="3">{{ $user['about'] }}</textarea>
 						<div class="form-text">Brief description for your profile.</div>
 					</div>
 
 					<!-- Save button -->
 					<div class="d-sm-flex justify-content-end">
-						<button type="button" class="btn btn-primary mb-0">Save changes</button>
+						<button type="submit" class="btn btn-primary mb-0">Save changes</button>
 					</div>
 				</form>
 			</div>
@@ -88,40 +101,44 @@
 						<h5 class="card-header-title mb-0">Social media profile</h5>
 					</div>
 					<!-- Card body START -->
+					<form action="{{ route('instructor.socmed.update', Auth::user()->id) }}" method="POST">
+						@csrf
+						@method('PUT')
 					<div class="card-body">
 						<!-- Facebook username -->
 						<div class="mb-3">
 							<label class="form-label"><i class="fab fa-facebook text-facebook me-2"></i>Enter facebook
 								username</label>
-							<input class="form-control" type="text" value="loristev" placeholder="Enter username">
+							<input class="form-control" type="text" name="social_facebook" value="{{ $user['social_facebook'] }}" placeholder="Enter username">
 						</div>
 
 						<!-- Twitter username -->
 						<div class="mb-3">
 							<label class="form-label"><i class="bi bi-twitter text-twitter me-2"></i>Enter twitter
 								username</label>
-							<input class="form-control" type="text" value="loristev" placeholder="Enter username">
+							<input class="form-control" type="text" name="social_twitter" value="{{ $user['social_twitter'] }}" placeholder="Enter username">
 						</div>
 
 						<!-- Instagram username -->
 						<div class="mb-3">
 							<label class="form-label"><i class="fab fa-instagram text-instagram-gradient me-2"></i>Enter
 								instagram username</label>
-							<input class="form-control" type="text" value="loristev" placeholder="Enter username">
+							<input class="form-control" type="text" name="social_instagram" value="{{ $user['social_instagram'] }}" placeholder="Enter username">
 						</div>
 
 						<!-- Youtube -->
 						<div class="mb-3">
 							<label class="form-label"><i class="fab fa-youtube text-youtube me-2"></i>Add your youtube
 								profile URL</label>
-							<input class="form-control" type="text" value="https://www.youtube.com/in/Eduport-05620abc"
+							<input class="form-control" type="text" name="social_youtube" value="{{ $user['social_youtube'] }}"
 								placeholder="Enter username">
 						</div>
 
 						<!-- Button -->
 						<div class="d-flex justify-content-end mt-4">
-							<button type="button" class="btn btn-primary mb-0">Save changes</button>
+							<button type="submit" class="btn btn-primary mb-0">Save changes</button>
 						</div>
+					</form>
 					</div>
 					<!-- Card body END -->
 				</div>
@@ -136,17 +153,20 @@
 						<h5 class="card-header-title mb-0">Update password</h5>
 					</div>
 					<!-- Card body START -->
+					<form action="{{ route('instructor.password.update', Auth::user()->id) }}" method="POST">
+						@csrf
+						@method('PUT')
 					<div class="card-body">
 						<!-- Current password -->
 						<div class="mb-3">
 							<label class="form-label">Current password</label>
-							<input class="form-control" type="password" placeholder="Enter current password">
+							<input class="form-control" type="password" name="password" placeholder="Enter current password">
 						</div>
 						<!-- New password -->
 						<div class="mb-3">
 							<label class="form-label"> Enter new password</label>
 							<div class="input-group">
-								<input class="form-control" type="password" placeholder="Enter new password">
+								<input class="form-control" type="password" id="new-password" name="new_password" autocomplete="new-password" placeholder="Enter new password">
 								<span class="input-group-text p-0 bg-transparent">
 									<i class="far fa-eye cursor-pointer p-2 w-40px"></i>
 								</span>
@@ -156,12 +176,13 @@
 						<!-- Confirm password -->
 						<div>
 							<label class="form-label">Confirm new password</label>
-							<input class="form-control" type="password" placeholder="Enter new password">
+							<input class="form-control" id="password_confirmation" autocomplete="new-password" type="password" placeholder="Enter new password">
 						</div>
 						<!-- Button -->
 						<div class="d-flex justify-content-end mt-4">
-							<button type="button" class="btn btn-primary mb-0">Change password</button>
+							<button type="submit" class="btn btn-primary mb-0">Change password</button>
 						</div>
+					</form>
 					</div>
 					<!-- Card body END -->
 				</div>
@@ -191,7 +212,7 @@
 								account</label>
 						</div>
 						<a href="#" class="btn btn-success-soft mb-2 mb-sm-0">Keep my account</a>
-						<a href="#" class="btn btn-danger mb-0">Delete my account</a>
+						<a href="{{ route('instructor.delete') }}" class="btn btn-danger mb-0">Delete my account</a>
 					</div>
 				</div>
 				<!-- Title and select END -->
